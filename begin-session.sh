24f345b43eb61b1f0a2b86cb17118b60d5591bbe
Alicia Boya García <ntrrgc@gmail.com>
2017-07-27 18:58:27 +0200

    Support X11 connections

--- begin-session.sh
+++ begin-session.sh
@@ -4,3 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME="$(perl -ne '/\[(.*)\]/ && print $1' "$DIR/schroot-config/schroot.conf")"
 
 schroot --begin-session -c "$NAME" -n "$NAME-session"
+
+# Allow X11 clients to use the server outside the chroot
+sudo cp -f "$HOME/.Xauthority" "$DIR/chroot/" || true
+cat << EOF | sudo tee "$DIR/chroot/etc/profile.d/x11.sh" > /dev/null
+export XAUTHORITY=/.Xauthority
+export DISPLAY="${DISPLAY:-}"
+EOF
