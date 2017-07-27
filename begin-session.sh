58395016b2197eba15d3fffb6dfae8145e18c6d8
Alicia Boya García <ntrrgc@gmail.com>
2017-07-28 00:51:49 +0200

    Make /dev/shm world-writable inside the chroot

--- begin-session.sh
+++ begin-session.sh
@@ -11,3 +11,7 @@ cat << EOF | sudo tee "$DIR/chroot/etc/profile.d/x11.sh" > /dev/null
 export XAUTHORITY=/.Xauthority
 export DISPLAY="${DISPLAY:-}"
 EOF
+
+# Make /dev/shm world-writable inside the chroot.
+# devtmpfs mounts may start out with the shm directory as read only.
+sudo chmod 777 "$DIR/chroot/dev/shm"
