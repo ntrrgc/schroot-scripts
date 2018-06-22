ce366127f34fdb6bd01959d577ac6a9be1c90398
Alicia Boya García <ntrrgc@gmail.com>
2018-06-22 16:32:11 +0200

    Add forced end session

--- end-session.sh
+++ end-session.sh
@@ -3,4 +3,8 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME="$(perl -ne '/\[(.*)\]/ && print $1' "$DIR/schroot-config/schroot.conf")"
 
-schroot --end-session -c "$NAME-session"
+schroot --end-session -c "$NAME-session" || (
+  # Force end session (e.g. if previously shut down without calling this script)
+  sudo rmdir /var/lib/schroot/mount/ntrrgc-webkit-session/
+  sudo rm /var/lib/schroot/session/ntrrgc-webkit-session
+)
