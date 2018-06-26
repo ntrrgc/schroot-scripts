04471a8455f101401ec52d48eba56acfb3001eac
Alicia Boya García <ntrrgc@gmail.com>
2018-06-26 13:06:29 +0200

    Fix edge case in ./end-session.sh

--- end-session.sh
+++ end-session.sh
@@ -5,6 +5,6 @@ NAME="$(perl -ne '/\[(.*)\]/ && print $1' "$DIR/schroot-config/schroot.conf")"
 
 schroot --end-session -c "$NAME-session" || (
   # Force end session (e.g. if previously shut down without calling this script)
-  sudo rmdir /var/lib/schroot/mount/ntrrgc-webkit-session/
+  sudo rmdir /var/lib/schroot/mount/ntrrgc-webkit-session/ || true
   sudo rm /var/lib/schroot/session/ntrrgc-webkit-session
 )
